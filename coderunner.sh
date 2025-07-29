19bd3f8157a3bd3770fc0d13038ce8954141025a
Shannon Pasto <shannonpasto@me.com>
2025-07-29 11:54:20 +1000

    added coderunner

---
+++ coderunner.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+
+appInstallPath="/Applications"
+bundleName="CodeRunner"
+installedVers=$(/usr/bin/defaults read "${appInstallPath}"/"${bundleName}.app"/Contents/Info.plist CFBundleShortVersionString 2>/dev/null)
+
+currentVers=$(/usr/bin/curl -s curl 'https://coderunnerapp.com/appcast.xml' | /usr/bin/xmllint --xpath '//rss/channel/item[1]/title/text()' - | /usr/bin/awk '{print $2}')
+downloadURL="https://coderunner.nyc3.cdn.digitaloceanspaces.com/CodeRunner-${currentVers}.zip"
+FILE=${downloadURL##*/}
+
+# compare version numbers
+if [ "${installedVers}" ]; then
+  installedVersNoDots=$(/bin/echo "${installedVers}" | /usr/bin/sed 's/\.//g')
+  currentVersNoDots=$(/bin/echo "${currentVers}" | /usr/bin/sed 's/\.//g')
+
+  # pad out currentVersNoDots to match installedVersNoDots
+  installedVersNoDotsCount=${#installedVersNoDots}
+  currentVersNoDotsCount=${#currentVersNoDots}
+
+  while [ "${currentVersNoDotsCount}" -lt "${installedVersNoDotsCount}" ]; do
+    currentVersNoDots="${currentVersNoDots}0"
+    currentVersNoDotsCount=$((currentVersNoDotsCount + 1))
+  done
+
+  if [ "${installedVersNoDots}" -ge "${currentVersNoDots}" ]; then
+    /bin/echo "${bundleName} does not need to be updated"
+    exit 0
+  else
+    /bin/echo "${bundleName} needs to be updated"
+  fi
+else
+  /bin/echo "Installing ${bundleName}"
+fi
+
+if /usr/bin/curl --retry 3 --retry-delay 0 --retry-all-errors -sL "${downloadURL}" -o /tmp/"${FILE}"; then
+  /bin/rm -rf "${appInstallPath}"/"${bundleName}.app" >/dev/null 2>&1
+  /usr/bin/ditto -xk /tmp/"${FILE}" "${appInstallPath}"/.
+  /usr/sbin/chown -R root:admin "${appInstallPath}"/"${bundleName}.app"
+  /bin/chmod -R 755 "${appInstallPath}"/"${bundleName}.app"
+  /bin/rm /tmp/"${FILE}"
+fi
